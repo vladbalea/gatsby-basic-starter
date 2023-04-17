1364f521bab59c81e76c2b2eaabfb413c08b2fca
vladbalea <vladd.balea@gmail.com>
2023-04-17 17:20:12 +0300

    added custom breakpoints in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
         "./src/components/**/*.{js,jsx,ts,tsx}"
     ],
     theme: {
+        screens: {
+            "compact": {"max": "600px"}, // 99.96% of phones in portrait
+            "medium": {"min": "600px", "max": "840px"}, // 93.73% of tablets in portrait, large unfolded inner displays in portrait
+            "expanded": {"min": "840px"}, // 97.22% of tablets in landscape, large unfolded inner displays in landscape
+        },
         extend: {},
     },
     plugins: [],
